d9b3ca1ec71fce0c5051fc57deab9f04d5dea1cf
Teri Phillips <phillips.terianne@gmail.com>
2023-01-17 05:10:28 -0500

    added javacript for form and event listener, cleaned up html formatting

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,11 +14,12 @@ var getFortuneCookie = function () {
 }
 //   execute fortune cookie function 
 getFortuneCookie();
-// fortune cookie function
-var getRecipes = function () {
-    //   requestGeoUrl = 'https://api.spoonacular.com/recipes/findByIngredients?ingredients=chicken&apiKey=de671bfd997d4e2bb42b9b72c3e38959';
+// recipes function
+var getRecipes = function (ingredients) {
+    //   requestUrl = 'https://api.spoonacular.com/recipes/findByIngredients?ingredients=' + ingredients +',&apiKey=de671bfd997d4e2bb42b9b72c3e38959';
 
     fetch('./assets/script/query.json')
+        //   fetch (requestUrl)
         .then(function (response) {
             return response.json();
         })
@@ -27,9 +28,6 @@ var getRecipes = function () {
             extractData(data);
         })
 }
-//   execute get recipes function 
-getRecipes();
-
 var extractData = function (data) {
     for (var i = 0; i < data.length; i++) {
         var title = data[i].title;
@@ -39,3 +37,14 @@ var extractData = function (data) {
         //TO DO: Mathieu - create function that renders recipes to the html index page by passing the variables title and imageUrl
     }
 }
+var formEl = document.querySelector('form');
+var formSubmit = function (event) {
+    event.preventDefault();
+    var ingredientsEl = document.getElementById('ingredients-search');
+    var ingredients = ingredientsEl.value;
+    //execute get recipes function
+    getRecipes(ingredients);
+}
+//event listener for the search
+formEl.addEventListener('submit', formSubmit);
+
